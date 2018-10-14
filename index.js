34270094872eacbb14174add816ebcaaecc943e8
Katrina Pritchard <pritckat@gmail.com>
2018-10-14 20:04:22 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,27 @@ const getFirstSelector = function(selector) {
 const nestedTarget = function() {
   return document.querySelector('#nested .target');
 }
+
+const increaseRankBy = function(n) {
+  const rankedLists = document.querySelectorAll('.ranked-list')
+
+  for (let i = 0, l = rankedLists.length; i < l; i++) {
+    let children = rankedLists[i].children
+
+    for (let j = 0, k = children.length; j < k; j++) {
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n
+    }
+  }
+}
+
+const deepestChild = function() {
+  let node = document.getElementById('grand-node')
+  let nextNode = node.children[0]
+
+  while (nextNode) {
+    node = nextNode
+    nextNode = node.children[0]
+  }
+
+  return node
+}
