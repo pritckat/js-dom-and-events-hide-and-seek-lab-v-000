096960d73b3eb8ad5669038d49f6fe221f9ac251
Katrina Pritchard <pritckat@gmail.com>
2018-10-14 20:01:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,15 @@ const getFirstSelector = function(selector) {
 const nestedTarget = function() {
   return document.querySelector('#nested .target');
 }
+
+const increaseRankBy = function(n) {
+  const rankedLists = document.querySelectorAll('.ranked-list')
+
+  for (let i = 0, l = rankedLists.length; i < l, i++) {
+    let children = rankedLists[i].children
+
+    for (let j = 0, k = children.length; j < k; j++) {
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n
+    }
+  }
+}
